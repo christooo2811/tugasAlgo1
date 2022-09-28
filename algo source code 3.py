911ded13e585d009159cb5e18d008a0afd6fa461
Christian Samuel <113886171+christooo2811@users.noreply.github.com>
2022-09-28 15:44:55 +0700

    Add files via upload

---
+++ algo source code 3.py
@@ -0,0 +1,18 @@
+name = input("Tuliskan namamu :")
+print(f" Halo, {name}!")
+nim = input("NIM :")
+print(f"{nim}")
+school = input("Asal sekolah :")
+
+bil = int(input("Masukkan panjang sisi bujur sangkar:"))
+luas = bil * 2
+keliling = 4 * bil
+
+print(f"Luas bujur sangkar adalah {luas} meter persegi") 
+print(f"Keliling bujur sangkar adalah {keliling} meter")
+print(f"Terima kasih {name}, {nim}, dari {school} karena telah menggunakan program saya.")
+
+
+print(f"Programmer : {name}")
+print(f"NIM : {nim}")
+print("Program studi : Sistem Informasi")
